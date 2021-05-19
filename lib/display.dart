c1987542ab411dffc49857173b4fe9668a68cb95
Pablo384 <pabloreinoso0@gmail.com>
2021-05-19 17:44:23 -0400

    💪 Null safety support

--- lib/display.dart
+++ lib/display.dart
@@ -110,13 +110,13 @@ class Display {
   /// Each logical display has a unique id.
   /// The default display has id [DEFAULT_DISPLAY]
   /// </p>
-  int displayId = DEFAULT_DISPLAY;
+  int? displayId = DEFAULT_DISPLAY;
 
   /// Returns a combination of flags that describe the capabilities of the display.
   /// @return The display flags.
   ///
   /// See [FLAG_SUPPORTS_PROTECTED_BUFFERS], [FLAG_SECURE], [FLAG_PRIVATE]
-  int flag;
+  int? flag;
 
   /// Returns the rotation of the screen from its "natural" orientation.
   /// The returned value may be [ROTATION_0]
@@ -130,7 +130,7 @@ class Display {
   /// degrees counter-clockwise, to compensate rendering will be rotated by
   /// 90 degrees clockwise and thus the returned value here will be
   /// [ROTATION_90].
-  int rotation;
+  int? rotation;
 
   /// Gets the name of the display.
   /// <p>
@@ -138,7 +138,7 @@ class Display {
   /// </p>
   ///
   /// @return The display's name.
-  String name;
+  String? name;
 
   Display({this.displayId, this.flag, this.name, this.rotation});
 }
