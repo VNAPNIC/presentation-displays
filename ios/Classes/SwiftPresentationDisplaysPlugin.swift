1b12954f350240dfbc26fea516ae6888b109a876
Ebram <>
2021-08-15 11:00:47 -0700

    listDisplay for iOS

--- ios/Classes/SwiftPresentationDisplaysPlugin.swift
+++ ios/Classes/SwiftPresentationDisplaysPlugin.swift
@@ -5,30 +5,30 @@ public class SwiftPresentationDisplaysPlugin: NSObject, FlutterPlugin {
     static var additionalWindows = [UIScreen:UIWindow]()
     static var screens = [UIScreen]()
     var flutterEngineChannel:FlutterMethodChannel=FlutterMethodChannel()
-    
-    
+
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "presentation_displays_plugin", binaryMessenger: registrar.messenger())
         let instance = SwiftPresentationDisplaysPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
-        
+
         screens.append(UIScreen.main)
-        
+
         NotificationCenter.default.addObserver(forName: UIScreen.didConnectNotification,
                                                object: nil, queue: nil) {
             notification in
-            
+
             // Get the new screen information.
             let newScreen = notification.object as! UIScreen
             let screenDimensions = newScreen.bounds
-            
+
             // Configure a window for the screen.
             let newWindow = UIWindow(frame: screenDimensions)
             newWindow.screen = newScreen
-            
+
             // You must show the window explicitly.
             newWindow.isHidden = true
-            
+
             // Save a reference to the window in a local array.
             self.screens.append(newScreen)
             self.additionalWindows[newScreen]=newWindow
@@ -37,7 +37,21 @@ public class SwiftPresentationDisplaysPlugin: NSObject, FlutterPlugin {
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         //    result("iOS " + UIDevice.current.systemVersion)
-        if call.method=="showPresentation"{
+        if  call.method=="listDisplay"
+        {
+            var  jsonDisplaysList:String = "[";
+            let screensList = SwiftPresentationDisplaysPlugin.screens
+            for i in 0..<screensList.count {
+                jsonDisplaysList+="{\"displayId\":"+String(i)+", \"name\":\"Screen "+String(i)+"\"},"
+            }
+
+            jsonDisplaysList = String(jsonDisplaysList.dropLast())
+            jsonDisplaysList+="]"
+            jsonDisplaysList = jsonDisplaysList.replacingOccurrences(of: "Screen 0", with: "Built-in Screen")
+            print(jsonDisplaysList)
+            result(jsonDisplaysList)
+        }
+        else if call.method=="showPresentation"{
             let args = call.arguments as? String
             let data = args?.data(using: .utf8)!
                 do {
@@ -57,22 +71,31 @@ public class SwiftPresentationDisplaysPlugin: NSObject, FlutterPlugin {
                     else if call.method=="transferDataToPresentation"{
                     self.flutterEngineChannel.invokeMethod("DataTransfer", arguments: call.arguments)
                 }
+            else
+            {
+                result(FlutterMethodNotImplemented)
+            }
+
         }
 
         private  func showPresentation(index:Int, routerName:String )
         {
-            let screen=SwiftPresentationDisplaysPlugin.screens[index]
-            let window=SwiftPresentationDisplaysPlugin.additionalWindows[screen]
-            
-            // You must show the window explicitly.
-            window?.isHidden=false
-            
-            let extVC = FlutterViewController()
-            extVC.setInitialRoute(routerName)
-            window?.rootViewController = extVC
-            
-            
-            self.flutterEngineChannel = FlutterMethodChannel(name: "presentation_displays_plugin_engine", binaryMessenger: extVC.binaryMessenger)
+            if index>0 && index < SwiftPresentationDisplaysPlugin.screens.count && SwiftPresentationDisplaysPlugin.additionalWindows.keys.contains(SwiftPresentationDisplaysPlugin.screens[index])
+            {
+                let screen=SwiftPresentationDisplaysPlugin.screens[index]
+                let window=SwiftPresentationDisplaysPlugin.additionalWindows[screen]
+
+                // You must show the window explicitly.
+                window?.isHidden=false
+
+                let extVC = FlutterViewController()
+                extVC.setInitialRoute(routerName)
+                window?.rootViewController = extVC
+
+
+                self.flutterEngineChannel = FlutterMethodChannel(name: "presentation_displays_plugin_engine", binaryMessenger: extVC.binaryMessenger)
+            }
+
         }
 
     }
