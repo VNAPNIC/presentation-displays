1e55c578c9763e570918ac747970984b059f144a
DESKTOP-5I89Q3O\vnapn <vn.apnic@Gmail.com>
2022-03-05 21:17:23 +0700

    fixed android 31

--- android/src/main/kotlin/com/namit/presentation_displays/DisplayJson.kt
+++ android/src/main/kotlin/com/namit/presentation_displays/DisplayJson.kt
@@ -6,5 +6,4 @@ package com.namit.presentation_displays
 data class DisplayJson(val displayId: Int,
                        val flags: Int,
                        val rotation: Int,
-                       val name: String){
-}
+                       val name: String)
--- test/presentation_displays_test.dart
+++ test/presentation_displays_test.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  const MethodChannel channel = MethodChannel('presentation_displays');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-}
+void main() {}
