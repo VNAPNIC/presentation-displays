ae22221b001e5508146e306156b1f16a29a0f30d
nambh <nambh@vps.com.vn>
2022-03-10 10:21:34 +0700

    update version

--- test/presentation_displays_test.dart
+++ test/presentation_displays_test.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('presentation_displays');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect('42', '42');
+  });
+}
